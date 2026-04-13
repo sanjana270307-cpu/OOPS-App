346a706c45a85cab976a72a5750d184eb1c9e4ad
sanjana270307-cpu <sanjana270307@gmail.com>
2026-04-13 23:48:47 +0530

    Coded UC2 to print OOPS in banner format

--- OOPSBannerApp.java
+++ OOPSBannerApp.java
@@ -1,3 +1,9 @@
+/**
+ * OOPSBannerApp
+ * This program prints OOPS to the console.
+ * @author Sanjana
+ * @version 1.0
+ */
 public class OOPSBannerApp {
     public static void main(String[] args) {
         System.out.println("OOPS");
