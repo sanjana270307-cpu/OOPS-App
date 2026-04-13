171dbffeab4fbaef5ef4c9f3c15f1a6d9c49f3d2
sanjana270307-cpu <sanjana270307@gmail.com>
2026-04-13 23:57:13 +0530

    Coded UC4 using array and loop for banner

---
+++ OOPSBannerUC4.java
@@ -0,0 +1,18 @@
+public class OOPSBannerUC4 {
+    public static void main(String[] args) {
+
+        String[] lines = {
+            "  *****     *****     ******    *****  ",
+            " *     *   *     *    *     *  *     * ",
+            "*       * *       *   *     *  *       ",
+            "*       * *       *   ******    *****  ",
+            "*       * *       *   *              * ",
+            " *     *   *     *    *        *     * ",
+            "  *****     *****     *         *****  "
+        };
+
+        for(int i = 0; i < lines.length; i++) {
+            System.out.println(lines[i]);
+        }
+    }
+}
